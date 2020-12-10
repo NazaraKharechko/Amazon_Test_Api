84eabdf78b5e8bf046f85058f6b591b945b6709f
Admin <nazarkharechko90@gmail.com>
2020-12-10 22:03:42 +0200

    + запис в db

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from mws import mws
 import MySQLdb
 
+
 db = MySQLdb.connect(host='localhost',
                      user='root',
                      passwd='root',
                      db='test_database')
 
-cur = db.cursor()
+cursor = db.cursor()
 
-access_key = ''  # replace with your access key
-seller_id = ''  # replace with your seller id
-secret_key = ''  # replace with your secret key
+access_key = 'AKIAJVFZKBOQE3FIY6XA'  # replace with your access key
+seller_id = 'A1EISHIC3JDGR2'  # replace with your seller id
+secret_key = 'NA2DSBf3/46qdMO1BzNQ1nBW4owAhB2oXFfzOIzS'  # replace with your secret key
 marketplace_usa = 'ATVPDKIKX0DER'
 
 orders_api = mws.Orders(access_key, secret_key, seller_id, region='US')
-orders = orders_api.list_orders(marketplaceids=[marketplace_usa], created_after='2020-12-10', max_results='1')
+orders = orders_api.list_orders(marketplaceids=[marketplace_usa], created_after='2020-11-10', max_results='10')
 
 products_as_dict = orders.parsed
 print(products_as_dict)
@@ -41,9 +42,10 @@ for y in products_as_dict['Orders']['Order']:
 
 s = products_as_dict['Orders']['Order']['PurchaseDate']['value']
 
-print(f'PurchaseDate => {s}')
-
-cur.execute(s)
+cursor.execute("CREATE TABLE dani (products_as_dict INT NOT NULL)")
+query = "INSERT INTO dani (products_as_dict) VALUES (%s)"
+values = ({s})
+cursor.execute(query, values)
 db.commit()
 
 db.close()
